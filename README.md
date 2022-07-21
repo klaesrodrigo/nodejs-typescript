3c3212252defbf910f62aca53d7c369f0a91f0ef
Rodrigo Klaes <rodrigoklaes@gmail.com>
2022-07-21 16:11:47 -0300

    Add database config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+## Run mongo in docker
 
+docker run -p 27017:27017 -d mongo
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import { Server } from '@overnightjs/core';
 import { Application, json } from 'express';
 import { ForecastController } from './controllers/forecast';
+import * as database from './database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.setupDatabase();
   }
 
   private setupExpress(): void {
@@ -19,6 +21,14 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
